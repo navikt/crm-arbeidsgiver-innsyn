f6324f4774d871f3543332fbf37f7f3fed642009
Andre Colle <56640917+raac09@users.noreply.github.com>
2023-12-19 14:27:20 +0100

    Edit script

--- bin/install-scratch.sh
+++ bin/install-scratch.sh
@@ -50,124 +50,6 @@ installDependencies() {
     sf dependency install --installationkeys "${keys}" --targetusername "$org_alias" --targetdevhubusername "$devHubAlias" || { error $? '"sf dependency install" command failed.'; }
 }
 
-deployingMetadata() {
-    if [[ $npm_config_without_deploy ]]; then
-        echo "Skipping..."
-    else
-        sf project deploy start || { error $? '"sf project deploy start" command failed.'; }
-    fi
-}
-
-assignPermission() {
-    sf org assign permset \
-    --name Create_reports_and_dashboards \
-    --name Arbeidsgiver_WarningWrite \
-    --name Arbeidsgiver_Create_and_share_reportfolders \
-    --name Arbeidsgiver_Kampanje \
-    --name Arbeidsgiver_NavApp \
-    --name Arbeidsgiver_NavTask \
-    --name Arbeidsgiver_Sykefravaer \
-    --name Arbeidsgiver_arenaActivity \
-    --name Arbeidsgiver_base \
-    --name Arbeidsgiver_contract \
-    --name Arbeidsgiver_opportunity \
-    --name Arbeidsgiver_temporaryLayoffs \
-    --name Arbeidsgiver_IA \
-    --name ArbeidsgiverStillinger \
-    --name Admin_Base \
-    --name CRM_LoginFlow \
-    || { error $? '"sf org assign permset" command failed.'; }
-}
-
-insertingTestData() {
-    # Creating temporary folder for test data files.
-    echo "Moving test data to temp folder..."
-    mkdir -p dummy-data/temp
-    cp -r dummy-data/activityTimeline dummy-data/temp
-    cp -r dummy-data/tag dummy-data/temp
-    echo ""
-
-    # Getting the Record Types from the new scratch org.
-    echo "Getting Record Types..."
-    sf data query --query "SELECT Id, SobjectType, DeveloperName FROM RecordType WHERE IsActive=true ORDER BY SObjectType, DeveloperName" --result-format json > dummy-data/temp/RecordTypes.json
-    echo ""
-
-    # Prepering Activity Timeline test data by replacing RecordType placeholders with correct Ids.
-    echo "Prepering Activity Timeline test data..."
-    echo "Prepering Account test data..."
-    for p in $(jq '.result.records[] | select(.SobjectType=="Account") | .DeveloperName' dummy-data/temp/RecordTypes.json);
-    do
-        minTest=$(sed -e 's/^"//' -e 's/"$//' <<<"$p");
-        replace="\$R{RecordType.Account.$(sed -e 's/^"//' -e 's/"$//' <<<"$p")}"
-        replacewith=$(sed -e 's/^"//' -e 's/"$//' <<<"$(jq '.result.records[] | select(.SobjectType=="Account" and .DeveloperName=="'$minTest'") | .Id' dummy-data/temp/RecordTypes.json)");
-        sed -i "" "s/$replace/$replacewith/g" "dummy-data/temp/activityTimeline/Account.json"
-    done
-    echo ""
-
-    echo "Prepering Custom Opportunities test data..."
-    for p in $(jq '.result.records[] | select(.SobjectType=="CustomOpportunity__c") | .DeveloperName' dummy-data/temp/RecordTypes.json);
-    do
-        minTest=$(sed -e 's/^"//' -e 's/"$//' <<<"$p");
-        replace="\$R{RecordType.CustomOpportunity__c.$(sed -e 's/^"//' -e 's/"$//' <<<"$p")}"
-        replacewith=$(sed -e 's/^"//' -e 's/"$//' <<<"$(jq '.result.records[] | select(.SobjectType=="CustomOpportunity__c" and .DeveloperName=="'$minTest'") | .Id' dummy-data/temp/RecordTypes.json)");
-        sed -i "" "s/$replace/$replacewith/g" "dummy-data/temp/activityTimeline/CustomOpportunities.json"
-    done
-    echo ""
-    echo "Activity Timeline test data prepared..."
-    echo ""
-
-    # Prepering Tag test data by replacing RecordType placeholders with correct Ids.
-    echo "Prepering Tag test data..."
-    echo "Prepering Account test data..."
-    for p in $(jq '.result.records[] | select(.SobjectType=="Account") | .DeveloperName' dummy-data/temp/RecordTypes.json);
-    do
-        minTest=$(sed -e 's/^"//' -e 's/"$//' <<<"$p");
-        replace="\$R{RecordType.Account.$(sed -e 's/^"//' -e 's/"$//' <<<"$p")}"
-        replacewith=$(sed -e 's/^"//' -e 's/"$//' <<<"$(jq '.result.records[] | select(.SobjectType=="Account" and .DeveloperName=="'$minTest'") | .Id' dummy-data/temp/RecordTypes.json)");
-        sed -i "" "s/$replace/$replacewith/g" "dummy-data/temp/tag/Accounts-B.json"
-    done
-
-    for p in $(jq '.result.records[] | select(.SobjectType=="Account") | .DeveloperName' dummy-data/temp/RecordTypes.json);
-    do
-        minTest=$(sed -e 's/^"//' -e 's/"$//' <<<"$p");
-        replace="\$R{RecordType.Account.$(sed -e 's/^"//' -e 's/"$//' <<<"$p")}"
-        replacewith=$(sed -e 's/^"//' -e 's/"$//' <<<"$(jq '.result.records[] | select(.SobjectType=="Account" and .DeveloperName=="'$minTest'") | .Id' dummy-data/temp/RecordTypes.json)");
-        sed -i "" "s/$replace/$replacewith/g" "dummy-data/temp/tag/Accounts-J.json"
-    done
-
-    for p in $(jq '.result.records[] | select(.SobjectType=="Account") | .DeveloperName' dummy-data/temp/RecordTypes.json);
-    do
-        minTest=$(sed -e 's/^"//' -e 's/"$//' <<<"$p");
-        replace="\$R{RecordType.Account.$(sed -e 's/^"//' -e 's/"$//' <<<"$p")}"
-        replacewith=$(sed -e 's/^"//' -e 's/"$//' <<<"$(jq '.result.records[] | select(.SobjectType=="Account" and .DeveloperName=="'$minTest'") | .Id' dummy-data/temp/RecordTypes.json)");
-        sed -i "" "s/$replace/$replacewith/g" "dummy-data/temp/tag/Accounts-O.json"
-    done
-    echo ""
-
-    echo "Prepering Custom Opportunities test data..."
-    for p in $(jq '.result.records[] | select(.SobjectType=="CustomOpportunity__c") | .DeveloperName' dummy-data/temp/RecordTypes.json);
-    do
-        minTest=$(sed -e 's/^"//' -e 's/"$//' <<<"$p");
-        replace="\$R{RecordType.CustomOpportunity__c.$(sed -e 's/^"//' -e 's/"$//' <<<"$p")}"
-        replacewith=$(sed -e 's/^"//' -e 's/"$//' <<<"$(jq '.result.records[] | select(.SobjectType=="CustomOpportunity__c" and .DeveloperName=="'$minTest'") | .Id' dummy-data/temp/RecordTypes.json)");
-        sed -i "" "s/$replace/$replacewith/g" "dummy-data/temp/tag/CustomOpportunities.json"
-    done
-    echo ""
-
-    echo "Tag test data prepared..."
-    echo ""
-
-    # Inserting the prepared test data
-    echo "Inserting test data..."
-    sf data import tree --plan  dummy-data/temp/activityTimeline/plan.json || { error $? '"sf data import tree" command failed.'; }
-    sf data import tree --plan  dummy-data/temp/tag/plan.json || { error $? '"sf data import tree" command failed.'; }
-    echo ""
-
-    echo "Removing temporary files..."
-    rm -rf dummy-data/temp
-    echo ""
-}
-
 #runPostInstallScripts() {
 #    sf apex run --file ./scripts/apex/activateMock.cls || { error $? '"sf apex run" command failed for Apex class: "activateMock".'; }
 #    sf apex run --file ./scripts/apex/createPortalUser.cls || { error $? '"sf apex run" command failed for Apex class: "createPortalUser".'; }
@@ -244,7 +126,7 @@ command -v jq >/dev/null 2>&1 || {
     exit 1
 }
 
-ORG_ALIAS="arbeidsgiver-base"
+ORG_ALIAS="arbeidsgiver-innsyn"
 secret=$npm_config_package_key
 devHubAlias=$(sf config get target-dev-hub --json | jq -r '.result[0].value')
 
@@ -254,16 +136,16 @@ else
     org_alias=$ORG_ALIAS
 fi
 
-echo "Installing crm-arbeidsgiver-base scratch org ($org_alias)"
+echo "Installing crm-arbeidsgiver-innsyn scratch org ($org_alias)"
 echo ""
 
 operations=(
     cleaningPreviousScratchOrg
     creatingScratchOrg
     installDependencies
-    deployingMetadata
-    assignPermission
-    insertingTestData
+    #deployingMetadata
+    #assignPermission
+    #insertingTestData
     #runPostInstallScripts
     #publishCommunity
     openOrg
@@ -273,9 +155,9 @@ operationNames=(
     "Cleaning previous scratch org"
     "Creating scratch org"
     "Installing dependencies"
-    "Deploying/Pushing metadata"
-    "Assigning permissions"
-    "Inserting test data"
+    #"Deploying/Pushing metadata"
+    #"Assigning permissions"
+    #"Inserting test data"
     #"Running post install scripts"
     #"Publishing arbeidsgiver-dialog site"
     "Opening org"
